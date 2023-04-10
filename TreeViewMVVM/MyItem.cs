f2583752491629815d11565073ccd0e99af411fe
Alexandra18042003 <emilia.lamba@student.unitbv.ro>
2023-04-10 18:28:01 +0300

    Add project files.

---
+++ TreeViewMVVM/MyItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TreeViewMVVM
+{
+    internal class MyItem : BaseVM
+    {
+        public MyItem() 
+        { 
+            SubCollection = new ObservableCollection<MyItem>();
+        }
+        public ObservableCollection<MyItem> SubCollection { get; set; }
+        private string itemName;
+        public string ItemName
+        {
+            get
+            {
+                return itemName;
+            }
+            set
+            {
+                itemName = value;
+                NotifyPropertyChanged("ItemName");
+            }
+        }
+    }
+}
---
+++ TreeViewMVVM/TreeViewVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewMVVM
+{
+    internal class TreeViewVM : BaseVM
+    {
+        public TreeViewVM()
+        {
+            ItemsCollection = new ObservableCollection<MyItem>();
+            ItemsCollection.Add(new MyItem
+            {
+                ItemName = "a",
+                SubCollection = new ObservableCollection<MyItem>()
+                {
+                    new MyItem { ItemName = "b", SubCollection = new ObservableCollection<MyItem>()
+                    {
+                        new MyItem() { ItemName = "d", SubCollection = new ObservableCollection<MyItem>() },
+                        new MyItem() { ItemName = "e", SubCollection = new ObservableCollection<MyItem>() }
+                    }
+                    },
+                    new MyItem { ItemName = "c", SubCollection = new ObservableCollection<MyItem>()
+                    {
+                        new MyItem() { ItemName = "f", SubCollection = new ObservableCollection<MyItem>() },
+                        new MyItem() { ItemName = "g", SubCollection = new ObservableCollection<MyItem>() }
+                    }
+                    }
+                }
+            });
+            ItemsCollection.Add(new MyItem()
+            {
+                ItemName = "h",
+                SubCollection = new ObservableCollection<MyItem>()
+                {
+                    new MyItem { ItemName = "i", SubCollection = new ObservableCollection<MyItem>()
+                    {
+                        new MyItem() { ItemName = "j", SubCollection = new ObservableCollection<MyItem>() }
+                    }
+                    }
+                }
+            });
+        }
+        public ObservableCollection<MyItem> ItemsCollection { get; set; }
+        private MyItem selectedItem;
+        public MyItem SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                selectedItem = value;
+                NotifyPropertyChanged("SelectedItem");
+            }
+        }
+    }
+}
